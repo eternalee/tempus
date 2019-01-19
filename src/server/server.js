b8232f6c1364a8f5154bd9c79abcbfae58627beb
eternalee <one.eternity@gmail.com>
2019-01-19 02:52:27 -0800

    server and testdata

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,32 @@
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-app.get('/', function (req, res) {
-  res.send({ hello: 'there' })
+const Controller = require('./Controller');
+const DoctorController = require('./DoctorController');
+const PatientController = require('./PatientController');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.listen(8080, () => console.log('Listening on port 8080'));
+
+app.post('/api/signin', Controller.authenticate, Controller.addCookieJWT)
+
+app.get('/api/patientlist', DoctorController.getPatientList)
+app.get('/api/patient/:name', DoctorController.getPatient)
+app.get('/api/patient/:name/appointments', DoctorController.getPatientAppointments)
+app.post('/api/patient/:name/appointments', DoctorController.schedulePatientAppointment)
+app.get('/api/patient/:name/files', DoctorController.getPatientFiles)
+app.post('/api/patient/:name/files', DoctorController.uploadPatientFile)
+app.delete('/api/patient/:name/files', DoctorController.removePatientFile)
+
+app.get('/test', function (req, res) {
+  res.send({ msg: "Routes are working" })
 })
 
-app.listen(3000, () => 'Listening at http://localhost:3000')
+//app.use(express.static('../../')); 
