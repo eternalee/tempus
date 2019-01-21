bd90168ff0d6e75256067455f4133aa22b56d3d3
eternalee <one.eternity@gmail.com>
2019-01-21 04:51:56 -0800

    calculate age

--- src/client/components/PatientProfile.jsx
+++ src/client/components/PatientProfile.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
+let calculateAge = (dob) => {
+  if (dob !== undefined) {
+    let dob2 = new Date(dob.replace(/-/g, "/"));
+    let today = new Date();
+    let years = today.getFullYear() - dob2.getFullYear();
+    if (today.getMonth() < dob2.getMonth() ||
+      today.getMonth() == dob2.getMonth() && today.getDate() < dob2.getDate()) {
+      years--;
+    }
+    return years;
+  }
+}
+
 const PatientProfile = ({ name, patient_id, dob, email, address, phone }) => {
 
   return (
     <div className='PatientProfile' >
       <p>Name: {name}</p>
       <p>Id: {patient_id}</p>
-      <p>Age: {dob}</p>
+      <p>Age: {calculateAge(dob)}</p>
       <p>Email: {email}</p>
       <p>Address: {address}</p>
       <p>Phone: {phone}</p>
-    </div>
+    </div >
   )
 };
 
